ee06b4ea4f31a6c896ed6bf6094cf3dcbf3ca960
F1GIAN <91794502+F1GIAN@users.noreply.github.com>
2021-12-12 01:26:33 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,6 +1,6 @@
 #ifndef esercitazione8_LIB_H
 #define esercitazione8_LIB_H
 
-float numeroMassimo(cost float numeri[], int dimensione);
+float numeroMassimo(const float numeri[], int dimensione);
 
 #endif esercitazione8_LIB_H
